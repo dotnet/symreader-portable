7d224f397cd74b6efb4fba2895f7a078a86f6aa0
Nick Guerrera <nicholg@microsoft.com>
2016-08-15 15:04:08 -0700

    Enable native embedded source tests

--- src/Microsoft.DiaSymReader.PortablePdb.Tests/Resources/EmbeddedSource.cmd
+++ src/Microsoft.DiaSymReader.PortablePdb.Tests/Resources/EmbeddedSource.cmd
@@ -2,6 +2,5 @@ csc /target:library /debug:portable /optimize- /deterministic /pathmap:%~dp0=C:\
 copy /y EmbeddedSource.pdb EmbeddedSource.pdbx
 copy /y EmbeddedSource.dll EmbeddedSource.dllx
 
-@REM -- TODO: Native support not there yet, hence no /embed, add it when implemented and unskip EmbeddedSource_Native test.
-csc /target:library /debug+ /optimize- /deterministic /pathmap:%~dp0=C:\ EmbeddedSource.cs EmbeddedSourceSmall.cs
+csc /target:library /debug+ /optimize- /deterministic /pathmap:%~dp0=C:\ /embed EmbeddedSource.cs EmbeddedSourceSmall.cs
 
