a96fe6d11cfe45da66a91b714a278d094c4d3b6d
Tomas Matousek <tomat@microsoft.com>
2016-12-19 14:58:36 +0100

    Update to the latest xunit

--- build/Scripts/windows/build.ps1
+++ build/Scripts/windows/build.ps1
@@ -18,7 +18,7 @@ Param(
   [string] $target = "Build",
   [string] $testFilter = "*.UnitTests.dll",
   [string] $integrationTestFilter = "*.IntegrationTests.dll",
-  [string] $xUnitVersion = "2.1.0"
+  [string] $xUnitVersion = "2.2.0-beta4-build3444"
 )
 
 function Create-Directory([string[]] $path) {
