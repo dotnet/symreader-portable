1e1e758d389e4cecbc21d62cf8fff968278f4bb7
Tomas Matousek <tomat@microsoft.com>
2017-10-13 15:54:10 -0700

    Build with Portable PDBs

--- build/build.ps1
+++ build/build.ps1
@@ -10,12 +10,45 @@ Param(
   [switch] $sign,
   [switch] $pack,
   [switch] $ci,
-  [switch] $clearCaches,
+  [switch] $prepareMachine,
+  [switch] $log,
+  [switch] $help,
   [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
 )
 
 set-strictmode -version 2.0
 $ErrorActionPreference = "Stop"
+
+function Print-Usage() {
+    Write-Host "Common settings:"
+    Write-Host "  -configuration <value>  Build configuration Debug, Release"
+    Write-Host "  -verbosity <value>      Msbuild verbosity (q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic])"
+    Write-Host "  -help                   Print help and exit"
+    Write-Host ""
+
+    Write-Host "Actions:"
+    Write-Host "  -restore                Restore dependencies"
+    Write-Host "  -build                  Build solution"
+    Write-Host "  -rebuild                Rebuild solution"
+    Write-Host "  -test                   Run all unit tests in the solution"
+    Write-Host "  -sign                   Sign build outputs"
+    Write-Host "  -pack                   Package build outputs into NuGet packages and Willow components"
+    Write-Host ""
+
+    Write-Host "Advanced settings:"
+    Write-Host "  -solution <value>       Path to solution to build"
+    Write-Host "  -ci                     Set when running on CI server"
+    Write-Host "  -log                    Enable logging (by default on CI)"
+    Write-Host "  -prepareMachine         Prepare machine for CI run"
+    Write-Host ""
+    Write-Host "Command line arguments not listed above are passed thru to msbuild."
+    Write-Host "The above arguments can be shortened as much as to be unambiguous (e.g. -co for configuration, -t for test, etc.)."
+}
+
+if ($help -or (($properties -ne $null) -and ($properties.Contains("/help") -or $properties.Contains("/?")))) {
+  Print-Usage
+  exit 0
+}
 
 $RepoRoot = Join-Path $PSScriptRoot "..\"
 $DotNetRoot = Join-Path $RepoRoot ".dotnet"
@@ -51,35 +84,47 @@ function InstallDotNetCli {
   }
 }
 
-function Build {  
-  if ($ci) {
+function Build {
+  if ($ci -or $log) {
     Create-Directory($logDir)
-    $binlog = "/bl:" + (Join-Path $LogDir "Build.binlog")
+    $logCmd = "/bl:" + (Join-Path $LogDir "Build.binlog")
   } else {
-    $binlog = ""
+    $logCmd = ""
   }
-  
-  & $DotNetExe msbuild $BuildProj /m /v:$verbosity $binlog /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:Build=$build /p:Rebuild=$rebuild /p:Test=$test /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci $properties
 
-  if ($lastExitCode -ne 0) {
-    throw "Build failed (exit code '$lastExitCode')."
+  & $DotNetExe msbuild $BuildProj /m /nologo /clp:Summary /warnaserror /v:$verbosity $logCmd /p:Configuration=$configuration /p:SolutionPath=$solution /p:Restore=$restore /p:Build=$build /p:Rebuild=$rebuild /p:Test=$test /p:Sign=$sign /p:Pack=$pack /p:CIBuild=$ci $properties
+}
+
+function Stop-Processes() {
+  Write-Host "Killing running build processes..."
+  Get-Process -Name "dotnet" -ErrorAction SilentlyContinue | Stop-Process
+  Get-Process -Name "vbcscompiler" -ErrorAction SilentlyContinue | Stop-Process
+}
+
+try {
+  if ($ci) {
+    Create-Directory $TempDir
+    $env:TEMP = $TempDir
+    $env:TMP = $TempDir
+  }
+
+  if ($restore) {
+    InstallDotNetCli
+  }
+
+  Build
+  exit $lastExitCode
+}
+catch {
+  Write-Host $_
+  Write-Host $_.Exception
+  Write-Host $_.ScriptStackTrace
+  exit 1
+}
+finally {
+  Pop-Location
+  if ($ci -and $prepareMachine) {
+    Stop-Processes
   }
 }
 
-if ($ci) {
-  Create-Directory $TempDir
-  $env:TEMP = $TempDir
-  $env:TMP = $TempDir
-}
-
-# clean nuget packages -- necessary to avoid mismatching versions of swix microbuild build plugin and VSSDK on Jenkins
-$nugetRoot = (Join-Path $env:USERPROFILE ".nuget\packages")
-if ($clearCaches -and (Test-Path $nugetRoot)) {
-  Remove-Item $nugetRoot -Recurse -Force
-}
-
-if ($restore) {
-  InstallDotNetCli
-}
-
-Build
