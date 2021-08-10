7a3e1d0e33480e60ed9c3e7d6fd7daaaf462c372
tmat <tomas.matousek@microsoft.com>
2021-08-10 10:13:09 -0700

    Revert resource changes

--- src/Microsoft.DiaSymReader.PortablePdb.Tests/Resources/EmbeddedSource.cs
+++ src/Microsoft.DiaSymReader.PortablePdb.Tests/Resources/EmbeddedSource.cs
@@ -1,6 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the License.txt file in the project root for more information.
+// should be higher than compression threshold (200 chars)
 
 using System;
 
