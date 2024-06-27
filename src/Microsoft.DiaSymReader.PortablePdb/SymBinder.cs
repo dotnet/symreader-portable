aebdd816330a42d6f4514a6736ee4ba4b199bbf5
Aaron R Robinson <arobins@microsoft.com>
2024-06-27 13:43:54 -0700

    Remove unused attribute

--- src/Microsoft.DiaSymReader.PortablePdb/SymBinder.cs
+++ src/Microsoft.DiaSymReader.PortablePdb/SymBinder.cs
@@ -2,12 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the License.txt file in the project root for more information.
 
-#if NET9_0_OR_GREATER
-global using GeneratedWhenPossibleComInterfaceAttribute = System.Runtime.InteropServices.Marshalling.GeneratedComInterfaceAttribute;
-#else
-global using GeneratedWhenPossibleComInterfaceAttribute = System.Runtime.InteropServices.ComImportAttribute;
-#endif
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
